5b7301a0041f063f58375de3e9a6596228d9314e
Roman Kolesnev <rvkolesnev@gmail.com>
2016-08-11 17:06:01 +0300

    Add logging for rails' dependency loading

---
+++ lib/heavy_control/extensions/logging.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+module HeavyControl
+  module Extensions
+    module Logging
+      def load_missing_constant(from_mod, const_name)
+        hc_log "Load missing constant '#{const_name}' from #{from_mod}"
+
+        super(from_mod, const_name)
+      end
+
+      def require_or_load(file_name, const_path = nil)
+        hc_log "Require of load '#{file_name}' with const_path '#{const_path}'"
+
+        super(file_name, const_path)
+      end
+
+      def search_for_file(path_suffix)
+        hc_log "Search for file with suffix '#{path_suffix}'"
+
+        super(path_suffix).tap do |result|
+          hc_log("and found '#{result}'")
+        end
+      end
+
+      private
+
+      def hc_log(msg)
+        Rails.logger.debug "HeavyControl: #{msg}"
+      end
+    end
+  end
+end
--- lib/heavy_control/railtie.rb
+++ lib/heavy_control/railtie.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 require 'rails'
 
+require 'heavy_control/extensions'
+
 module HeavyControl
   class Railtie < Rails::Railtie
+    initializer 'heavy_control.extensions' do
+      ActiveSupport::Dependencies.singleton_class.prepend Extensions::Logging
+    end
   end
 end
